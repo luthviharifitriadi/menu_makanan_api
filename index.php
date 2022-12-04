d6a610b76ef1250d55e6779c29ee3289445b0564
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2022-12-04 08:04:04 +0700

    membuat tampilan menu

--- index.php
+++ index.php
@@ -27,6 +27,31 @@
     </div>
   </nav>
 
+  <div class="container">
+    
+    <div class="row mt-3">
+      <div class="col">
+        <h1>ALL Menu</h1>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+          <img src="img/menu/american-favourite.jpg" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">American Favoutite</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">RP. 60.000,-</h5>
+            <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+ 
 
 
 
